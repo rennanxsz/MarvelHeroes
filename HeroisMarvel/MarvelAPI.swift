4dd47f8bd3e556901121ecd96c66ff5be528d441
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-27 21:43:47 -0300

    Creating Marvel Classes
    Criando as classes da Marvel a serem utilizadas

---
+++ HeroisMarvel/MarvelAPI.swift
@@ -0,0 +1,13 @@
+//
+//  MarvelAPI.swift
+//  HeroisMarvel
+//
+//  Created by Rennan Bruno on 27/03/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+class MarvelAPI {
+    
+}
---
+++ HeroisMarvel/MarvelClasses.swift
@@ -0,0 +1,9 @@
+//
+//  MarvelClasses.swift
+//  HeroisMarvel
+//
+//  Created by Rennan Bruno on 27/03/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import Foundation
