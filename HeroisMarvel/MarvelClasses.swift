05bfdf04ad969d395a61faecedd9edabf1adc182
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-27 21:58:46 -0300

    Creating Structs
    Criando as struct para utilização.

--- HeroisMarvel/MarvelClasses.swift
+++ HeroisMarvel/MarvelClasses.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+struct MarvelInfo: Codable {
+    let code: Int
+    let Status: String
+    let data: MarvelData
+}
+
+struct MarvelData {
+    let offset: Int
+    let limit: Int
+    let total: Int
+    let count: Int
+    let results: [Hero]
+}
+
+struct Hero: Codable {
+    let id: Int
+    let name: String
+    let description: String
+    let thumbnail: Thumbnail
+    let url: [HeroURL]
+}
+
+struct Thumbnail: Codable {
+    let path: String
+    let ext: String
+    
+    enum CodinKeys: String, CodingKey {
+        case path
+        case ext = "extension"
+    }
+    
+}
+
+struct HeroURL: Codable {
+    let type: String
+    let url: String
+}
