de1c1f4908d75adf12ba623df7e4a27d2f6a653f
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-27 23:05:59 -0300

    Creating method loadHero
    Criando e configurando o método loadHeros para ser usado na chamada do personagem.

--- HeroisMarvel/MarvelAPI.swift
+++ HeroisMarvel/MarvelAPI.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SwiftHash
+import Alamofire
 
 class MarvelAPI {
     
@@ -16,7 +17,25 @@ class MarvelAPI {
     static private let publicKey = "4c2c1934cbcd638fbb6417a58938a939"
     static private let limit = 50
     
-    class func loadHeros(name: String?) {
+    class func loadHeros(name: String?, page: Int = 0, onComplete: @escaping (MarvelInfo?) -> Void) {
+        let offset = page * limit
+        let startsWith: String
+        if let name = name, !name.isEmpty {
+            startsWith = "nameStartsWith=\(name.replacingOccurrences(of: " ", with: ""))&"
+        } else {
+            startsWith = ""
+        }
+        let url = basePath + "offset=\(offset)&limit=\(limit)&" + startsWith + getCredentials()
+        print(url)
+        AF.request(url).responseJSON { (response) in
+            guard let data = response.data,
+                    let marvelInfo = try? JSONDecoder().decode(MarvelInfo.self, from: data),
+                  marvelInfo.code == 200 else {
+                onComplete(nil)
+                return
+            }
+            onComplete(marvelInfo)
+        }
         
     }
     
